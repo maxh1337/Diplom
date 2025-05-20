2cb450f324dcffcc7a7acf883ab9fea825488374
Maksim Vorobev <max11052003@yandex.ru>
2025-05-20 23:40:58 +0300

    Forget to turn on mobile and desktop telegram only. When finished fixing bug :)

--- webapp-react/src/app/config/Providers.tsx
+++ webapp-react/src/app/config/Providers.tsx
@@ -3,12 +3,13 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type PropsWithChildren, useState } from "react";
 import { Toaster } from "react-hot-toast";
+import { usePlatformGuard } from "./YouCantAccesFromBrowser";
 
 export function Providers({ children }: PropsWithChildren) {
   const [client] = useState(new QueryClient());
 
-  // const guard = usePlatformGuard();
-  // if (guard) return guard;
+  const guard = usePlatformGuard();
+  if (guard) return guard;
 
   return (
     <QueryClientProvider client={client}>
